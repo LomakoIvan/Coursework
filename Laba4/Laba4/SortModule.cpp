05adb543865461f01bcd42375e03ef96a891494e
Hartchik <ivan.27.lamaka@gmail.com>
2025-04-29 12:56:05 +0300

    Back up to Insert Sort and before reverse Deck

--- Laba4/Laba4/SortModule.cpp
+++ Laba4/Laba4/SortModule.cpp
@@ -1,4 +1,5 @@
 #include "Applicants.h"
+#include "Memory.h"
 #include <iostream>
 
 using namespace std;
@@ -18,13 +19,16 @@ Node* GetNodeInDeck(Deck& deck, unsigned int node_number) {
 void InsertSortMark(Deck& deck) {
 	unsigned int node_count = deck.GetNodeCount();
 	if (node_count < 2) { return; }
-	
-	Node* current_node = GetNodeInDeck(deck, 1);
-	
-	for (int i = 1; current_node != nullptr; i++) {
-		Node* this_node = GetNodeInDeck(deck, i - 1);
-		Applicant key = current_node->data;
-		
+
+	Node* key_node = deck.get_front_node()->prev_ptr;
+
+	for (int i = 1; key_node != nullptr; i++) {
+		Applicant key = key_node->data;
+		Node* current_node = key_node;
+		Node* this_node = key_node->next_ptr;
+
+		if (!this_node) { return; }
+
 		int j = i - 1;
 		for (; j >= 0 && this_node->data.mark > key.mark; j--) {
 			current_node->data = this_node->data;
@@ -36,6 +40,6 @@ void InsertSortMark(Deck& deck) {
 			current_node->data = key;
 		}
 
-		current_node = GetNodeInDeck(deck, i + 1);
+		key_node = key_node->prev_ptr;
 	}
 }
